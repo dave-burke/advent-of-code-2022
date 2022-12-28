c44893368bee2d0369c70e85ce4ce3c21cf39068
Dave Burke <dburke84@gmail.com>
2022-12-28 14:42:58 -0600

    Get first unique window

--- src/aoc/day06.clj
+++ src/aoc/day06.clj
@@ -1,10 +1,25 @@
 (ns aoc.day06
   (:require [clojure.string]))
 
+;(defn positive-numbers
+	;([] (positive-numbers 1))
+	;([n] (lazy-seq (cons n (positive-numbers (inc n))))))
+
+(defn window-scanner
+  [window-size]
+  (fn scan [col]
+    (if (<= (count col) window-size)
+      col
+      (lazy-seq (cons (take window-size col) (scan (rest col)))))))
+
+(defn all-unique?
+  [col]
+  (= (count col) (count (set col))))
+
 (defn part1
   "Day 06 Part 1"
   [input]
-  input)
+  (first (filter all-unique? ((window-scanner 4) input))))
 
 (defn part2
   "Day 06 Part 2"
